6efa1c27099237566b09a8b3fddc03a68636b55e
msalmantheboss-rgb <m.salmantheboss@gmail.com>
2026-03-26 17:42:00 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,77 @@
+import streamlit as st
+import pickle
+import string
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import nltk
+from nltk.stem.porter import PorterStemmer
+
+nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+
+ps = PorterStemmer()
+
+
+def transform_text(text):
+    text = text.lower()
+    text = word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+
+
+st.title("Email Classifier")
+html_temp = """
+<div style="background-color:tomato;padding:10px">
+<h2 style="color:white;text-align:center;">Streamlit Email Spam Detector ML App </h2>
+</div>
+<br>
+  """
+st.markdown(html_temp, unsafe_allow_html=True)
+
+input_sms = st.text_area("Enter the message")
+
+if st.button('Inquire'):
+    # 1. preprocess
+    transformed_sms = transform_text(input_sms)
+    # 2. vectorize
+    vector_input = tfidf.transform([transformed_sms])
+    # 3. predict probability
+    prob = model.predict_proba(vector_input)[0]
+    spam_prob = prob[1] * 100
+    ham_prob = prob[0] * 100
+
+
+    # Optional: display final label
+    result = model.predict(vector_input)[0]
+    if result == 1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
+    
+     # 4. Display the probabilities
+    st.write(f"Spam Probability: {spam_prob:.2f}%")
+    st.write(f"Not Spam Probability: {ham_prob:.2f}%")
